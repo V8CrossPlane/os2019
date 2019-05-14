0b8ad0d18e6e855d81f90cd46b754759fcb25201
V8CrossPlane <linhtd.b8099@st.usth.edu.vn>
2019-05-14 13:44:39 +0700

    Practical (fixed)

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -3,29 +3,36 @@
 #include <unistd.h>
 #include <wait.h>
 
-int main(){
+int main()
+{
   int status, pid1, pid2;
   printf("This is main before fork()\n");
   pid1 = fork();
-  if(pid1 == 0){
+  if(pid1 == 0)
+  {
     printf("\tThis is the first child\n\tNow run \"ps -ef\"\n");
     char* args[] = {"/bin/ps", "-ef", NULL};
-    if(execvp("/bin/ps", args) < 0){
+    if(execvp("/bin/ps", args) < 0)
+	{
       printf("Fail to execute \"ps -ef\"\n");
     }
   }
-  else {
+  else 
+  {
     waitpid(pid1, NULL, 0);
     printf("This is parent after fork, child is %d\n\tContinue to create another child\n", pid1);
     pid2 = fork();
-    if(pid2 == 0){
+    if(pid2 == 0)
+	{
       printf("\tThis is the second child\n\tNow run \"free -h\"\n");
       char* args[] = {"/usr/bin/free", "-h", NULL};
-      if(execvp("/usr/bin/free", args) < 0){
+      if(execvp("/usr/bin/free", args) < 0)
+	  {
         printf("Fail to execute \"free -h\"\n");
       }
     }
-    else {
+    else 
+	{
       printf("This is parent after 2 forks, second child is %d\nNothing to do more.\n", pid2);
       waitpid(pid2, NULL, 0);
     }
--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -6,24 +6,27 @@
 #define BUFFER_SIZE 10
 
 
-typedef struct {
-  char type; // 0=fried chicken, 1=French fries
-  int amount; // pieces or weight
-  char unit; // 0=piece, 1=gram
+typedef struct 
+{
+  char type;
+  int amount;
+  char unit;
 } item;
 
 item buffer[BUFFER_SIZE];
 int first = 0;
 int last = 0;
 
-void printLog(){
+void printLog()
+{
   printf("first = %d\t last = %d\n", first, last);
   return;
 }
 
-void produce(item *i){
-  while ((first + 1) % BUFFER_SIZE == last){
-    //do nothing -- no free buffer item
+void produce(item *i)
+{
+  while ((first + 1) % BUFFER_SIZE == last)
+  {
     printf("No free buffer item!\n");
   }
   memcpy(&buffer[first], i, sizeof(item));
@@ -31,10 +34,11 @@ void produce(item *i){
   printLog();
 }
 
-item* consume(){
+item* consume()
+{
   item* i = malloc(sizeof(item));
-  while (first == last){
-    // do nothing -- nothing to consume
+  while (first == last)
+  {
     printf("Nothing to consume!\n");
   }
   memcpy(i, &buffer[last], sizeof(item));
@@ -43,7 +47,8 @@ item* consume(){
   return i;
 }
 
-item* initItem(char type, int amount, char unit){
+item* initItem(char type, int amount, char unit)
+{
   item* i = malloc(sizeof(item));
   i->type = type;
   i->amount = amount;
@@ -51,34 +56,35 @@ item* initItem(char type, int amount, char unit){
   return i;
 }
 
-//produce routine
-void *produceThread(void *param){
+void *produceThread(void *param)
+{
   printf("This is produce thread\n");
   produce(initItem('1',3,'0'));
   produce(initItem('0',9,'1'));
   produce(initItem('0',3,'0'));
 }
 
-//consume routine
-void *consumeThread(void *param){
+void *consumeThread(void *param)
+{
   printf("This is consume thread\n");
   consume();
   consume();
 }
 
-int main(){
+int main()
+{
   pthread_t tid1, tid2;
   printf("Initial log:\nfirst = %d\tlast = %d\n\n", first, last);
 
-  //create two new threads
-  if(pthread_create(&tid1, NULL, produceThread, NULL) != 0){
+  if(pthread_create(&tid1, NULL, produceThread, NULL) != 0)
+  {
     perror("pthread_created() error");
   }
-  if(pthread_create(&tid2, NULL, consumeThread, NULL) != 0){
+  if(pthread_create(&tid2, NULL, consumeThread, NULL) != 0)
+  {
     perror("pthread_created() error");
   }
   
-  // wait for two threads finish
   pthread_join(tid1, NULL);
   pthread_join(tid2, NULL);
   return 0;
